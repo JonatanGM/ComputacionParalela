bd855e99eeec4574ba783f4c035e27e3dedbb182
Jonatan García <uo223428@uniovi.es>
2016-11-08 23:03:32 +0100

    Se incluye el soporte para openMP

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,14 +1,15 @@
 #include <jni.h>
 #include <string>
-//#include <omp.h>
+#include <omp.h>
 
 extern "C"
 jstring
 Java_uniovi_es_computacionparalela_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hola mundo";//omp_get_num_threads();
-    return env->NewStringUTF(hello.c_str());
+
+        std::string hello = "Hola mundo "+om();
+        return env->NewStringUTF(hello.c_str());
 }
 
 
